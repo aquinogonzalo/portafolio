e5e7b56f68617626476d76e52cb5795223696a5e
aquinogonzalo <aquinoalejandro666@gmail.com>
2023-05-08 12:52:45 -0300

    Se empezo a desarrollar la seccion stack, se agrego barras de progreso de Bootstrap y se solucionó un problema con la responsividad con una funcion de javascript

--- js/index.js
+++ js/index.js
@@ -8,3 +8,19 @@ function downScroll(){
 }
 
 window.addEventListener("scroll", downScroll);
+
+
+const home = document.getElementById("home");
+
+function viewportChange(){
+    const windowWidth = window.innerWidth;
+    if (windowWidth > 768) {
+        home.classList.add("vh-100");
+        home.classList.remove("viewport-normal");
+    } else {
+        home.classList.remove("vh-100");
+        home.classList.add("viewport-normal");
+    }
+}
+
+window.addEventListener("resize", viewportChange);
